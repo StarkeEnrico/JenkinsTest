d2423d4aa559d7d283c62f8a9b99f96ba445daf2
Jenkins <jenkins@localhost>
2021-02-15 11:49:57 +0000

    Added new batch of EC2 Instances.

--- softwareone-bit-devops/eu-central-1/ec2-windows-test.tf
+++ softwareone-bit-devops/eu-central-1/ec2-windows-test.tf
@@ -823,3 +823,69 @@ module "detvs01218" {
   }
 }
 
+module "detvs01217" {
+  source                   = "bitcloud-terraform.softwareone.com/BITCloud/module-ec2-instance-windows/aws"
+  version                  = "0.0.31"
+  instance_name            = "detvs01217"
+  instance_type            = "t3.micro"
+  description              = "Temporary Test server"
+  os_version               = "WindowsServer2019"
+  os_type                  = "Core"
+  domain_name              = "domain.global"
+  availability_zone        = "B"
+  service_level_class      = "D"
+  owned_by                 = "enrico.starke"
+  managed_by               = "enrico.starke"
+  created_by               = "BITCloud/job/Create_EC2_Windows_TST"
+  cost_center              = "15.10"
+  system_data_volume_size  = "40"
+  vpc_id                   = "vpc-02d82d26468e360b4"
+  nic_subnet_locator       = "Infrastructure"
+  nic_private_ip_addresses = [ "10.99.23.74" ]
+  security_group_scenario  = "Jenkins"
+  security_group_suffix    = [ "Local","Datacenter","WAN" ]
+  monitoring               = true
+  ssm_document_domainjoin  = "SWO-BCWindowsADJoin-domain-global-d-996728e3d6"
+  elb_instance_locator     = "n/a"
+  backup                   = true
+  cloudwatch_sns_topics     = []
+  additional_tags          = {
+      "aws-migration-project-id" = "MPE14143"
+      "map-migrated"             = "d-server-abcd1234"
+      "bit:Reference"            = "JenkinsPiplineTest"
+  }
+}
+
+module "detvs01218" {
+  source                   = "bitcloud-terraform.softwareone.com/BITCloud/module-ec2-instance-windows/aws"
+  version                  = "0.0.31"
+  instance_name            = "detvs01218"
+  instance_type            = "t3.micro"
+  description              = "Temporary Test server"
+  os_version               = "WindowsServer2019"
+  os_type                  = "Core"
+  domain_name              = "domain.global"
+  availability_zone        = "B"
+  service_level_class      = "D"
+  owned_by                 = "enrico.starke"
+  managed_by               = "enrico.starke"
+  created_by               = "BITCloud/job/Create_EC2_Windows_TST"
+  cost_center              = "15.10"
+  system_data_volume_size  = "40"
+  vpc_id                   = "vpc-02d82d26468e360b4"
+  nic_subnet_locator       = "Infrastructure"
+  nic_private_ip_addresses = [ "10.99.23.75" ]
+  security_group_scenario  = "Jenkins"
+  security_group_suffix    = [ "Local","Datacenter","WAN" ]
+  monitoring               = true
+  ssm_document_domainjoin  = "SWO-BCWindowsADJoin-domain-global-d-996728e3d6"
+  elb_instance_locator     = "n/a"
+  backup                   = true
+  cloudwatch_sns_topics     = []
+  additional_tags          = {
+      "aws-migration-project-id" = "MPE14143"
+      "map-migrated"             = "d-server-abcd1234"
+      "bit:Reference"            = "JenkinsPiplineTest"
+  }
+}
+
