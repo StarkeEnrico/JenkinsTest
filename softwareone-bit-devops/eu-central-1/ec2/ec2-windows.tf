3b94a7f84eb1e4e6bf8a0184f87909d149838b2f
Jenkins <jenkins@localhost>
2021-10-07 12:59:13 +0000

    Added new EC2 Instance . https://bitcloud-jenkins.domain.global/job/BITCloud/job/test/job/Get_EC2_definition_test_from_Consul/7/

---
+++ softwareone-bit-devops/eu-central-1/ec2/ec2-windows.tf
@@ -0,0 +1,5 @@
+module "dexvs01693" {
+    source                   = "bitcloud-terraform.softwareone.com/BITCloud/module-consul-windows/aws"
+    instance_name            = "dexvs01693"
+}
+            
