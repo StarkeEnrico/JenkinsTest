13c6f7022af78ee8ac7069a9bb071bee2cde055d
root <root@ip-10-99-17-84.eu-central-1.compute.internal>
2021-10-25 12:59:52 +0000

    Added new Subscription.

--- bit-master/subscription.tf
+++ bit-master/subscription.tf
@@ -1,2 +1,8 @@
 
 
+module "Application Services Delivery (Production)" {
+              source                   = "bitcloud-terraform.softwareone.com/BITCloud/module-ea-subscription/azure"
+              version                  = "0.1.1"
+              name                     = "Application Services Delivery (Production)"
+            }
+            
