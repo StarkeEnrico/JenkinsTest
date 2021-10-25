b65a0cb939d9729e51d664cfcd54b4660701d7a0
root <root@ip-10-99-17-249.eu-central-1.compute.internal>
2021-10-25 11:45:11 +0000

    Added new Subscription.

--- bit-master/subscription.tf
+++ bit-master/subscription.tf
@@ -11,3 +11,15 @@ module "Business Intelligence (Production)" {
               name                     = "Business Intelligence (Production)"
             }
             
+module "Application Services Delivery (Production)" {
+              source                   = "bitcloud-terraform.softwareone.com/BITCloud/module-ea-subscription/azure"
+              version                  = "0.1.1"
+              name                     = "Application Services Delivery (Production)"
+            }
+            
+module "Business Intelligence (Production)" {
+              source                   = "bitcloud-terraform.softwareone.com/BITCloud/module-ea-subscription/azure"
+              version                  = "0.1.1"
+              name                     = "Business Intelligence (Production)"
+            }
+            
