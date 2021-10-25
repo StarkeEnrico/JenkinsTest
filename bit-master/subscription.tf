7dc804c731a846b1162eafdc502d994f5adea10c
ES <56999583+StarkeEnrico@users.noreply.github.com>
2021-10-25 15:02:21 +0200

    Update subscription.tf

--- bit-master/subscription.tf
+++ bit-master/subscription.tf
@@ -1,8 +1,1 @@
 
-
-module "Application Services Delivery (Production)" {
-              source                   = "bitcloud-terraform.softwareone.com/BITCloud/module-ea-subscription/azure"
-              version                  = "0.1.1"
-              name                     = "Application Services Delivery (Production)"
-            }
-            
